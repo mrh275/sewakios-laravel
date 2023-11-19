586489e5b42b9a0a9504395761c566a6a3ba56c1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-19 08:25:59 +0700

    create sewa migration

---
+++ database/migrations/2023_11_19_012227_create_sewas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sewas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kios');
+            $table->string('nama_penyewa');
+            $table->string('no_hp');
+            $table->string('gedung');
+            $table->integer('harga_sewa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sewas');
+    }
+};
