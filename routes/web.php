4dfcfb79217378ff6fc21a4af64f3b25336f2780
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-11-21 21:11:12 +0700

    added list of routes for crud data sewa and ruko

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ListRukoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SewaController;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,13 @@ Route::get('/', [LoginController::class, 'index']);
 Route::get('/admin', [AdminController::class, 'index']);
 Route::get('/admin/sewa', [SewaController::class, 'index']);
 Route::get('/admin/sewa/tambah', [SewaController::class, 'tambah']);
+Route::post('/admin/sewa/store', [SewaController::class, 'store']);
+Route::get('/admin/sewa/edit/{nomorKontrak}', [SewaController::class, 'edit']);
+Route::get('/admin/sewa/hapus/{nomorKontrak}', [SewaController::class, 'hapus']);
+Route::post('/admin/sewa/update', [SewaController::class, 'update']);
+Route::get('/admin/ruko', [ListRukoController::class, 'index']);
+Route::get('/admin/ruko/tambah', [ListRukoController::class, 'tambah']);
+Route::post('/admin/ruko/store', [ListRukoController::class, 'store']);
+Route::get('/admin/ruko/edit/{no_unit}', [ListRukoController::class, 'edit']);
+Route::get('/admin/ruko/hapus/{no_unit}', [ListRukoController::class, 'hapus']);
+Route::post('/admin/ruko/update', [ListRukoController::class, 'update']);
